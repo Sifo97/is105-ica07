e5c6d01cf8b244523448df67705b1fe904906b85
uia-student <TestMail@example.com>
2017-05-15 17:38:35 +0200

    Added UDP Client and server

---
+++ UDP/Client/Client.go
@@ -0,0 +1,30 @@
+//https://varshneyabhi.wordpress.com/2014/12/23/simple-udp-clientserver-in-golang/
+package main
+ 
+import (
+    "fmt"
+    "net"
+)
+ 
+func CheckError(err error) {
+    if err  != nil {
+        fmt.Println("Error: " , err)
+    }
+}
+ 
+func main() {
+    ServerAddr,err := net.ResolveUDPAddr("udp","192.168.1.129:10001")
+    CheckError(err)
+ 
+    LocalAddr, err := net.ResolveUDPAddr("udp", "192.168.1.129:0")
+    CheckError(err)
+ 
+    Conn, err := net.DialUDP("udp", LocalAddr, ServerAddr)
+    CheckError(err)
+ 
+    defer Conn.Close()	
+        Conn.Write([]byte("Mote Fr 5.5 14:45 Flaaklypa"))
+        if err != nil {
+            fmt.Println(err)
+}
+}
